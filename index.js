2a2860243f4aa45a5d474671880d6afd73c5b60f
jasnmar <jasnmar@gmail.com>
2024-03-31 13:36:44 -0400

    Finished App

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const appSettings = {
     databaseURL: "https://realtime-database-51a33-default-rtdb.firebaseio.com/"
 }
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 setupPage()
 
 const app = initializeApp(appSettings)
@@ -26,23 +26,48 @@ function addBtnClick(event) {
     clearNewItem();
     console.log(inputVal)
 }
+
 onValue(shoppingListInDB, function(snapshot) {
-    clearShoppingList()
-    let itemsList = Object.values(snapshot.val())
-    for (let i = 0; i < itemsList.length; i++) {
-        addItemToShoppingList(itemsList[i])
-        console.log(itemsList[i])
+    if(snapshot.exists()) {
+        clearShoppingList()
+        let itemsList = Object.entries(snapshot.val())
+        console.log(itemsList)
+        for (let i = 0; i < itemsList.length; i++) {
+            const currentItem = itemsList[i]
+            const currentItemID = currentItem[0];
+            const currentItemValue = currentItem[1]
+            console.log(currentItemValue)
+            addItemToShoppingList(currentItem)
+            console.log(itemsList[i])
+        }
+
+    } else {
+        shoppingListEl.textContent = "No items here... yet"
     }
-    console.log(snapshot.val())
+
 })
+
 function clearNewItem() {
     inputFieldEl.value = ""
 }
 
 function addItemToShoppingList(item) {
-    shoppingListEl.innerHTML += `<li>${item}</li>`
+    //shoppingListEl.innerHTML += `<li>${item}</li>`
+    const newEl = document.createElement("li")
+    const itemID = item[0]
+    const itemVal = item[1]
+
+    newEl.addEventListener("click", function() {
+        console.log(itemID)
+        const exactLocationOfItem = ref(database, `shoppingList/${itemID}`)
+        remove(exactLocationOfItem)
+    })
+
+    newEl.textContent = itemVal
+    shoppingListEl.appendChild(newEl)
 }
 
 function clearShoppingList() {
     shoppingListEl.innerHTML = ""
 }
+
